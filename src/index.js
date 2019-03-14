c9154593038f5ab783202bc97b7ad5e7f2b3b09a
froliceric0914 <wzhao.eric@gmail.com>
2019-03-14 01:12:01 +0000

    add props

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+//pass the name/date as props to the child to render
+const avatar = faker.image.avatar();
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author="Alex" timeAgo="Today at 12:00" content="A" />
+      <CommentDetail author="Jane" timeAgo="Today at 16:00" content="B" />
+      <CommentDetail author="Sam" timeAgo="Today at 18:00" content="C" />
     </div>
   );
 };
